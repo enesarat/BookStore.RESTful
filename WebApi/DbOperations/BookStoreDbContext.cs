53d4e0a686240576e63b7e6bcc4b8e58ca55d993
Enes Arat <enes_arat@outlook.com>
2023-02-16 23:29:37 +0300

    Initial commit. Contains: Book controller has created. Book operations has provided with dto and viewModel entities.

---
+++ WebApi/DbOperations/BookStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApi.DbOperations
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
