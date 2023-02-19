94f310aba811567f894bc069abbc6571a3a4b1a8
Enes Arat <enes_arat@outlook.com>
2023-02-19 16:46:17 +0300

    Contains: Logger services added to project and has configured over DI Container.

--- WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -2,15 +2,18 @@
 using System.Diagnostics;
 using System.Net;
 using System.Xml;
+using WebApi.Services;
 
 namespace WebApi.Middlewares
 {
     public class CustomExceptionMiddleware
     {
         private readonly RequestDelegate _next;
-        public CustomExceptionMiddleware(RequestDelegate next)
+        private readonly ILoggerService _logger;
+        public CustomExceptionMiddleware(RequestDelegate next, ILoggerService logger)
         {
             _next = next;
+            _logger = logger;
         }
         public async Task Invoke(HttpContext context)
         {
@@ -18,11 +21,11 @@ namespace WebApi.Middlewares
             try
             {
                 string message = "[Request]  HTTP " + context.Request.Method + " - " + context.Request.Path;
-                Console.WriteLine(message);
+                _logger.Write(message);
                 await _next(context);
                 watch.Stop();
-                message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + "responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + "ms";
-                Console.WriteLine(message);
+                message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + "ms";
+                _logger.Write(message);
             }
             catch (Exception ex)
             {
@@ -39,7 +42,7 @@ namespace WebApi.Middlewares
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             string message = "[Error]    HTTP  " + context.Request.Method + " - " + context.Response.StatusCode + " Error Message " + ex.Message + " in " + watch.Elapsed.TotalMilliseconds + " ms ";
-            Console.WriteLine(message);
+            _logger.Write(message);
             var result = JsonConvert.SerializeObject(new { error = ex.Message }, Newtonsoft.Json.Formatting.None);
 
             return context.Response.WriteAsync(result);
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using WebApi.DbOperations;
 using WebApi.Middlewares;
+using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,9 @@ builder.Services.AddSwaggerGen(options =>
         },
     });
 });
+
+builder.Services.AddSingleton<ILoggerService, ConsoleLogger>();
+
 
 // Here, we define our tatabase context as "in memory database" before the build task of application
 builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDB"));
